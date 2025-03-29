9e8a31dfa19fe2d6e469ce8954684b79af7ec2e1
dangto <dang0898494156@gmail.com>
2025-03-29 20:32:55 +0700

    first commit

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddServices(configuration);
+            services.AddRepository();
+        }
+
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddScoped<IAuthenticationService, AuthenticationService>();
+            //services.AddScoped<Authentication>();
+            //services.AddScoped<ICourseService, CourseService>();
+            //services.AddScoped<INotificationService, NotificationService>();
+            //services.AddScoped<IChapterService, ChapterService>();
+            //services.AddScoped<IEnrollmentService, EnrollmentService>();
+            //services.AddScoped<ILessonService, LessonService>();
+            //services.AddScoped<ILabService, LabService>();
+            //services.AddScoped<IChapterProgressService, ChapterProgressSevice>();
+            //services.AddScoped<ISubmissionService, SubmissionService>();
+            //services.AddScoped<IPaymentService, PaymentService>();
+            //services.AddScoped<IVnPayService, VnPayService>();
+            //services.AddScoped<IDashboardService, DashboardService>();
+            //services.AddScoped<FcmService>();
+
+        }
+        public static void AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class UnitOfWork(KidProgramingDbContext dbContext) : IUnitOfWork
+    {
+        private bool disposed = false;
+        private readonly KidProgramingDbContext _dbContext = dbContext;
+
+        public IGenericRepository<T> GetRepository<T>() where T : class
+        {
+            return new GenericRepository<T>(_dbContext);
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void BeginTransaction()
+        {
+            _dbContext.Database.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _dbContext.Database.CommitTransaction();
+        }
+
+        public void RollBack()
+        {
+            _dbContext.Database.RollbackTransaction();
+        }
+    }
+}
